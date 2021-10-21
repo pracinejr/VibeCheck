2baba521aa339155c01afe3ee59c23e27eb6338e
Peter Racine Jr <peterracinejr@gmail.com>
2021-10-21 15:16:16 -0500

    added all models and thier corresponding properties

---
+++ VibeCheck/Models/Connection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VibeCheck.Models
+{
+    public class Connection
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int VenueId { get; set; }
+
+        public int MutualFriendId { get; set; }
+
+        [Required]
+        public int AcquaintanceId { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
--- VibeCheck/Models/Venue.cs
+++ VibeCheck/Models/Venue.cs
@@ -6,23 +6,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace VibeCheck.Models
 {
-    public class User
+    public class Venue
     {
         public int Id { get; set; }
 
         [Required]
-        [StringLength(28, MinimumLength = 28)]
-        public string FirebaseUserId { get; set; }
-
-        [Required]
         [MaxLength(50)]
         public string Name { get; set; }
-
-        [Required]
-        [MaxLength(255)]
-        public string Email { get; set; }
-
-        public string ImageUrl { get; set; }
-
     }
 }
