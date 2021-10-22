a2fa76d3a4fa28a63e3964c0f7369a6eddbf9164
Peter Racine Jr <peterracinejr@gmail.com>
2021-10-22 15:13:39 -0500

    added venue repo, irepo, and controller

---
+++ VibeCheck/Repositories/IVenueRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using VibeCheck.Models;
+
+
+namespace VibeCheck.Repositories
+{
+    public interface IVenueRepository
+    {
+        void AddVenue(Venue venue);
+        void DeleteVenue(int venueId);
+        List<Venue> GetAllVenues();
+        Venue GetVenueById(int id);
+        void UpdateVenue(Venue venue);
+    }
+}
